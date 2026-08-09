9af04a65f6a73d484c9fbdaabb3f75cb74bd9b18
Adelyn Ann Carr <adelyncarr123@gmail.com>
2026-08-09 21:36:51 +0800

    Add Lesson 03 input and output program

---
+++ Lesson03_InputAndOutput.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+int a,b;
+clrscr();
+printf("Enter first number: ");
+scanf("%i",&a);
+printf("Enter second number: ");
+scanf("%i",&b);
+printf("The first number is %i, next is %i",a,b);
+getch();}
