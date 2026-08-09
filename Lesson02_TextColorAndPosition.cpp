ee766f2f364a0486356f85a89db2645502ac38f0
Adelyn Ann Carr <adelyncarr123@gmail.com>
2026-08-09 21:21:38 +0800

    Add Lesson 02 text color and positioning

---
+++ Lesson02_TextColorAndPosition.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+clrscr();
+gotoxy(40,12);
+textcolor(4+BLINK);
+cprintf("Mondriaan Aura College");
+gotoxy(40,13);
+textcolor(BLUE+128);
+cprintf("College of IT");
+getch();}
