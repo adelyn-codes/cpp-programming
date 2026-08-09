c657e2cf91fc3dfdfc382871d5e70e77401cc4cf
Adelyn Ann Carr <adelyncarr123@gmail.com>
2026-08-09 21:30:12 +0800

    Update Lesson 02 with background colors

--- Lesson02_TextColorAndPosition.cpp
+++ Lesson02_TextColorAndPosition.cpp
@@ -4,8 +4,10 @@ main(){
 clrscr();
 gotoxy(40,12);
 textcolor(4+BLINK);
+textbackground(MAGENTA);
 cprintf("Mondriaan Aura College");
 gotoxy(40,13);
 textcolor(BLUE+128);
+textbackground(GREEN);
 cprintf("College of IT");
 getch();}
